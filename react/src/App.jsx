70fed59e337953616a1c52220dadbb22153b1503
estebantassi <esteban.tassi.perso@gmail.com>
2025-05-15 15:58:24 +0200

    server-side signup checks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,19 +4,31 @@ import axios from './api/axios';
 function App() {
 
   const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [emailcheck, setEmailcheck] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordcheck, setPasswordcheck] = useState("")
 
   const signupform = async (e) => {
     e.preventDefault()
 
+    //PERFORM CHECKS HERE
+
     try {
 
-      await axios.post('/signup', {
+      const response = await axios.post('/signup', {
         username: username,
+        email: email,
+        emailcheck: emailcheck,
+        password: password,
+        passwordcheck: passwordcheck
       })
+
+      console.log(response.data)
 
       setUsername("")
     } catch (err) {
-      console.log(err)
+      console.log(err.response.data)
     }
   }
 
@@ -27,6 +39,19 @@ function App() {
       <form onSubmit={(e) => signupform(e)}>
         <label>Username</label>
         <input value={username} onChange={(e) => setUsername(e.target.value)} />
+
+        <label>Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <label>Email Check</label>
+        <input value={emailcheck} onChange={(e) => setEmailcheck(e.target.value)} />
+
+        <label>Password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <label>Password Check</label>
+        <input value={passwordcheck} onChange={(e) => setPasswordcheck(e.target.value)} />
+
         <button>SIGNUP</button>
       </form>
     </>
--- server/requests/post/signup.js
+++ server/requests/post/signup.js
@@ -1,19 +1,44 @@
 const db = require('../../config/database')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+
+const IsEmailValid = require('../../tools/checks').IsEmailValid
 
 const Signup = async (req, res) => {
 
     if (!req.body) return res.status(400).json("Wrong request")
-    if (!req.body.username || !req.body.email || !req.body.emailcheck || !req.body.password || passwordcheck) return res.status(400).json("Please fill out all the necessary fields")
+    if (!req.body.username || !req.body.email || !req.body.emailcheck || !req.body.password || !req.body.passwordcheck) return res.status(400).json("Please fill out all the necessary fields")
 
+    const username = req.body.username
+    const email = req.body.email
+    const emailcheck = req.body.emailcheck
+    const password = req.body.password
+    const passwordcheck = req.body.passwordcheck
 
-        
+    if (username.length > process.env.MAX_USERNAME_LENGTH) return res.status(400).json("Username is too long")
+    if (username.length < process.env.MIN_USERNAME_LENGTH) return res.status(400).json("Username is too short")
+
+    if (password.length > process.env.MAX_PASSWORD_LENGTH) return res.status(400).json("Password is too long")
+    if (password.length < process.env.MIN_PASSWORD_LENGTH) return res.status(400).json("Password is too short")
+
+    if (password != passwordcheck) return res.status(400).json("Passwords don't match")
+    if (email != emailcheck) return res.status(400).json("Emails don't match")
+    if (!IsEmailValid(email)) return res.status(400).json("Email isn't valid")
+
+    const passwordsalt = await bcrypt.genSalt()
+    const cryptedpassword = await bcrypt.hash(password, passwordsalt)
+
     try {
         await db.query(`
-            INSERT INTO users (username)
-            VALUES (?)
-            `, [req.body.username])
+            INSERT INTO users (username, email, password)
+            VALUES (?, ?, ?)
+            `, [username, email, cryptedpassword])
 
+
+        return res.status(200).json("Account successfully created")
     } catch (err) {
+        if (err.errno == 1062) return res.status(400).json("This email is already taken")
+
         console.log(err)
     }
 
