8d4e2d4c15cc3f1ba12f6629499ff44be563a745
estebantassi <esteban.tassi.perso@gmail.com>
2025-09-16 01:36:00 +0200

    .

--- server/config/gcs.js
+++ server/config/gcs.js
@@ -1,5 +1,10 @@
 const { Storage } = require('@google-cloud/storage')
 require('dotenv').config()
+const fs = require('fs')
+
+const keyData = Buffer.from(process.env.GCP_KEY_BASE64, "base64")
+fs.writeFileSync("../key.json", keyData)
+
 const storage = new Storage({ keyFilename: process.env.KEYFILENAME })
 const bucket = storage.bucket(process.env.BUCKET_NAME)
 
