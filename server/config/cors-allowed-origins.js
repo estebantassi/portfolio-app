806a1931a1dc507761fe8b2704295bd9e5f307ec
estebantassi <esteban.tassi.perso@gmail.com>
2025-09-17 04:44:07 +0200

    cors actual fix

--- server/config/cors-allowed-origins.js
+++ server/config/cors-allowed-origins.js
@@ -1,17 +1,7 @@
 require('dotenv').config()
-const cors = require("cors")
 
 const allowedOrigins = process.env.WEBSITE_URL
   ? process.env.WEBSITE_URL.split(",").map(origin => origin.trim())
   : []
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true
-}))
+module.exports = allowedOrigins
