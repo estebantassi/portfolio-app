ea3850a708560a36bea176334f8cbe6c677ca8a7
estebantassi <esteban.tassi.perso@gmail.com>
2025-09-17 03:37:06 +0200

    hopefully final fix

--- server/config/gcs.js
+++ server/config/gcs.js
@@ -1,11 +1,14 @@
 const { Storage } = require('@google-cloud/storage')
 require('dotenv').config()
 const fs = require('fs')
+const path = require('path')
+
+const keyPath = path.join(__dirname, "..", "key.json")
 
 const keyData = Buffer.from(process.env.GCP_KEY_BASE64, "base64")
-fs.writeFileSync("../key.json", keyData)
+fs.writeFileSync(keyPath, keyData)
 
-const storage = new Storage({ keyFilename: process.env.KEYFILENAME })
+const storage = new Storage({ keyFilename: keyPath })
 const bucket = storage.bucket(process.env.BUCKET_NAME)
 
-module.exports = bucket
+module.exports = bucket;
